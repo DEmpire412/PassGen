0ce0c1e9c4e832a80674a096fe5287efa5770eea
DEmpire412 <davidempire412@gmail.com>
2023-03-13 14:36:52 -0400

    Initial Commit

---
+++ PassGen/PassGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassGenApp.swift
+//  PassGen
+//
+//  Created by David Empire on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PassGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
